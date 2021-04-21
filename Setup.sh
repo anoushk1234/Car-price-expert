cdeec64d311577764f9fd1c838b04a699486e3d9
Anoushk Kharangate <32778608+anoushk1234@users.noreply.github.com>
2021-04-21 20:00:51 +0530

    Rename Setup.sh to setup.sh

--- Setup.sh
+++
@@ -1,1 +0,0 @@
-mkdir -p ~/.streamlit/echo "\[general]\n\email = \"anoushka1828@gmail.com\"\n\" > ~/.streamlit/credentials.tomlecho "\[server]\n\headless = true\n\enableCORS=false\n\port = $PORT\n\" > ~/.streamlit/config.toml
---
+++ setup.sh
@@ -0,0 +1,1 @@
+mkdir -p ~/.streamlit/echo "\[general]\n\email = \"anoushka1828@gmail.com\"\n\" > ~/.streamlit/credentials.tomlecho "\[server]\n\headless = true\n\enableCORS=false\n\port = $PORT\n\" > ~/.streamlit/config.toml
