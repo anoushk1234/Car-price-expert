494f586be02cf31ceb6ae660fff1d1ddab646da9
Anoushk Kharangate <anoushka1828@gmail.com>
2021-04-21 20:26:03 +0530

    modified setupsh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,9 @@
-mkdir -p ~/.streamlit/echo "\[general]\n\email = \"anoushka1828@gmail.com\"\n\" > ~/.streamlit/credentials.tomlecho "\[server]\n\headless = true\n\enableCORS=false\n\port = $PORT\n\" > ~/.streamlit/config.toml
+mkdir -p ~/.streamlit/
+
+echo "\
+[server]\n\
+headless = true\n\
+port = $PORT\n\
+enableCORS = false\n\
+\n\
+" > ~/.streamlit/config.toml
